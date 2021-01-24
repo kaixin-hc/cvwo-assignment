1b98ca46166f63e5735fdc759d8cb21913a937cc
DistractedDaydreamer <chia.hannahk@gmail.com>
2021-01-24 21:39:54 +0800

    working CRUD

--- code files/app/models/task.rb
+++ code files/app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
     validates :title, presence: true
-    validates :deadline, presence: true
+    # validates :deadline, presence: true
 end
--- code files/config/routes.rb
+++ code files/config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/tasks', to: 'tasks#index'
-      post '/tasks', to: 'tasks#create'
-      get '/tasks/:id', to: 'tasks#show'
-      patch '/tasks/:id', to: 'tasks#update'
-      delete '/tasks/:id', to: 'tasks#destroy'
-      # resources :tasks
+      resources :tasks
+
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
